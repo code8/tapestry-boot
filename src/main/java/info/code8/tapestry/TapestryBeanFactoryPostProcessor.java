f956dba54a5ccd6936906b07db1a729abc5fa1ef
Luca Menegus <lucam@dbmsrl.com>
2017-03-12 19:08:05 +0100

    Better tapetsry/spring interaction
    - (the actual fix) Split Tapestry Registry initialization between
     initial Resgistry creation which can happen in Spring
     BeanFactoryPostProcessor and actual Registry initialization which,
     accordingly to spring BeanFactoryPostProcessor must happen at actual
     bean creation time. Basically we register the Registry (along with other
     Tapestry provided services) with spring  while we are customizeing
     the spring factory but we delay the Registry finalization till Spring
     ask us to provide the actual TapestryFilter.
     Here's the relevant excerpt from spring doc:
     "A BeanFactoryPostProcessor may interact with and modify bean definitions,
     but never bean instances"
     See: http://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/beans/factory/config/BeanFactoryPostProcessor.html
     for further details.
    
    - Split Tapestrey application context initializer from Tapestry
    BeanFactoryPostProcessor. To me this makes the code more clear.
    
    - Be more tolerant about the supplied context type. We must do this
     because when spring-boot is used in cloud environment (my specific use
     case is to include cloud-netflixspring-cloud-starter-feign dep to
     support feign based http clients) spring will create a parent
     non-web application context to configure the cloud stuff. We need to
     be able to cope with it (in my impl we just log a warning message,
     ignore it and hope that we will be invoked again with the real
     web appl ctx).

--- src/main/java/info/code8/tapestry/TapestryBeanFactoryPostProcessor.java
+++ src/main/java/info/code8/tapestry/TapestryBeanFactoryPostProcessor.java
@@ -1,5 +1,11 @@
 package info.code8.tapestry;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.internal.InternalConstants;
 import org.apache.tapestry5.internal.SingleKeySymbolProvider;
@@ -14,73 +20,88 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
-import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
-import org.springframework.web.context.ConfigurableWebApplicationContext;
-
-import java.util.*;
 
 /**
  * Created by code8 on 11/6/15.
  */
 
-public class TapestryBeanFactoryPostProcessor implements BeanFactoryPostProcessor, ApplicationContextInitializer {
-    private Logger logger = LoggerFactory.getLogger(getClass());
+public class TapestryBeanFactoryPostProcessor
+        implements BeanFactoryPostProcessor, Ordered {
 
-    protected ConfigurableWebApplicationContext applicationContext;
-    protected Registry registry;
+    private static final Logger logger = LoggerFactory.getLogger(TapestryBeanFactoryPostProcessor.class);
+    protected final EmbeddedWebApplicationContext applicationContext;
+    private Registry registry = null;
+    private TapestryAppInitializer appInitializer = null;
 
-    protected Registry makeTapestryRegistry(SymbolProvider symbolProvider, String filterName) {
-        String executionMode = symbolProvider.valueForSymbol(SymbolConstants.EXECUTION_MODE);
-        TapestryAppInitializer appInitializer = new TapestryAppInitializer(logger, symbolProvider, filterName, executionMode);
-        appInitializer.addModules(new SpringModuleDef(applicationContext));
-        appInitializer.addModules(AssetSourceModule.class);
-        Registry registry = appInitializer.createRegistry();
-        registry.performRegistryStartup();
-        appInitializer.announceStartup();
-        registry.cleanupThread();
-        return registry;
+    public TapestryBeanFactoryPostProcessor(EmbeddedWebApplicationContext applicationContext) {
+        super();
+        this.applicationContext = applicationContext;
+    }
+
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
     }
 
     @Override
-    public void initialize(ConfigurableApplicationContext applicationContext) {
-        if (applicationContext instanceof EmbeddedWebApplicationContext) {
-            this.applicationContext = (ConfigurableWebApplicationContext) applicationContext;
-            applicationContext.addBeanFactoryPostProcessor(this);
-        } else {
-            throw new RuntimeException("tapestry-spring-boot works only with EmbeddedWebApplicationContext (Supplied context class was" + applicationContext.getClass() + ")");
-        }
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        Collection<String> packagesToScan = findPackagesToScan(applicationContext);
+        String appModuleClass = findAppModule(packagesToScan, applicationContext.getEnvironment());
+        
+
+        String filterName = appModuleClass.substring(appModuleClass.lastIndexOf('.') + 1).replace("Module", "");
+        SymbolProvider combinedProvider = setupTapestryContext(appModuleClass, filterName);
+        String executionMode = combinedProvider.valueForSymbol(SymbolConstants.EXECUTION_MODE);
+        logger.info("TB: About to start Tapestry app module: {}, filterName: {}, executionMode: {} ", appModuleClass, filterName, executionMode);
+        appInitializer = new TapestryAppInitializer(logger, combinedProvider, filterName, executionMode);
+        appInitializer.addModules(new SpringModuleDef(applicationContext));
+        appInitializer.addModules(AssetSourceModule.class);
+        logger.info("TB: creating tapestry registry");
+        registry = appInitializer.createRegistry();
+        
+        beanFactory.addBeanPostProcessor(new TapestryFilterPostProcessor());
+
+        registerTapestryServices(applicationContext.getBeanFactory(),
+                combinedProvider.valueForSymbol(InternalConstants.TAPESTRY_APP_PACKAGE_PARAM) + ".services",
+                registry);
+
+        // This will scan and find TapestryFilter which in turn will be post
+        // processed be TapestryFilterPostProcessor completing tapestry initialisation
+        ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner((BeanDefinitionRegistry) applicationContext);
+        scanner.scan(TapestryBeanFactoryPostProcessor.class.getPackage().getName());
+
     }
 
-    protected Collection<String> findPackagesToScan(ConfigurableApplicationContext applicationContext) {
-        Set<String> packages = new HashSet<>();
-        Object springApplication = applicationContext.getBeansWithAnnotation(SpringBootApplication.class).values().iterator().next();
-        packages.add(springApplication.getClass().getPackage().getName());
-        return packages;
-    }
-
-    protected String findAppModule(Collection<String> packages, Environment environment) {
-        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false, environment);
-        scanner.addIncludeFilter(new AnnotationTypeFilter(TapestryApplication.class));
-
-        for(String pack : packages) {
-            Set<BeanDefinition> definitions = scanner.findCandidateComponents(pack);
-            if (!definitions.isEmpty()) {
-                return definitions.iterator().next().getBeanClassName();
+    private class TapestryFilterPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            if (bean.getClass() == TapestryFilter.class) {
+                logger.info("TB: About to start TapestryFilter, begin Registry initialization");
+                registry.performRegistryStartup();
+                registry.cleanupThread();
+                appInitializer.announceStartup();
+                logger.info("TB: About to start TapestryFilter, Registry initialization complete");
             }
+            return bean;
         }
 
-        throw new RuntimeException("TapestryApplication not found. Use @TapestryApplication to mark module.");
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            return bean;
+        }
+
     }
 
     protected SymbolProvider setupTapestryContext(String appModuleClass, String filterName) {
@@ -95,7 +116,7 @@ public class TapestryBeanFactoryPostProcessor implements BeanFactoryPostProcesso
         String executionMode = environment.getProperty(SymbolConstants.EXECUTION_MODE, "production");
         tapestryContext.put(SymbolConstants.EXECUTION_MODE, executionMode);
 
-        String rootPackageName = appModuleClass.substring(0, appModuleClass.lastIndexOf('.')).replace(".services","");
+        String rootPackageName = appModuleClass.substring(0, appModuleClass.lastIndexOf('.')).replace(".services", "");
         tapestryContext.put(InternalConstants.TAPESTRY_APP_PACKAGE_PARAM, rootPackageName);
 
         environment.getPropertySources().addFirst(new MapPropertySource("tapestry-context", tapestryContext));
@@ -107,31 +128,38 @@ public class TapestryBeanFactoryPostProcessor implements BeanFactoryPostProcesso
                 new SingleKeySymbolProvider(SymbolConstants.EXECUTION_MODE, executionMode));
     }
 
-    protected void registerTapestryServices(ConfigurableListableBeanFactory beanFactory, String servicesPackage) {
-        ServiceActivityScoreboard scoreboard = registry.getService(ServiceActivityScoreboard.class);
-        scoreboard.getServiceActivity().forEach(service-> {
-            if (service.getServiceInterface().getPackage().getName().startsWith(servicesPackage)
-                    || !service.getMarkers().isEmpty() || service.getServiceInterface().getName().contains("tapestry5")) {
-                Object proxy = registry.getService(service.getServiceId(), service.getServiceInterface());
-                beanFactory.registerResolvableDependency(service.getServiceInterface(), proxy);
-            }
-        });
-
-        beanFactory.registerResolvableDependency(Registry.class, registry);
+    protected Collection<String> findPackagesToScan(ConfigurableApplicationContext applicationContext) {
+        Set<String> packages = new HashSet<>();
+        Object springApplication = applicationContext.getBeansWithAnnotation(SpringBootApplication.class).values().iterator()
+                .next();
+        packages.add(springApplication.getClass().getPackage().getName());
+        return packages;
     }
 
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        Collection<String> packagesToScan = findPackagesToScan(applicationContext);
-        String appModuleClass = findAppModule(packagesToScan, applicationContext.getEnvironment());
-        logger.debug("Found tapestry app module: " + appModuleClass);
-        String filterName = appModuleClass.substring(appModuleClass.lastIndexOf('.') + 1).replace("Module", "");
-        SymbolProvider combinedProvider = setupTapestryContext(appModuleClass, filterName);
-        registry = makeTapestryRegistry(combinedProvider, filterName);
-        registerTapestryServices(applicationContext.getBeanFactory(),
-                combinedProvider.valueForSymbol(InternalConstants.TAPESTRY_APP_PACKAGE_PARAM) + ".services");
+    protected String findAppModule(Collection<String> packages, Environment environment) {
+        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false, environment);
+        scanner.addIncludeFilter(new AnnotationTypeFilter(TapestryApplication.class));
+        for (String pack : packages) {
+            Set<BeanDefinition> definitions = scanner.findCandidateComponents(pack);
+            if (!definitions.isEmpty()) {
+                return definitions.iterator().next().getBeanClassName();
+            }
+        }
+        throw new RuntimeException("TapestryApplication not found. Use @TapestryApplication to mark module.");
+    }
 
-        ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner((BeanDefinitionRegistry) applicationContext);
-        scanner.scan(getClass().getPackage().getName());
+    protected void registerTapestryServices(ConfigurableListableBeanFactory beanFactory, String servicesPackage,
+            Registry registry) {
+        ServiceActivityScoreboard scoreboard = registry.getService(ServiceActivityScoreboard.class);
+        scoreboard.getServiceActivity().forEach(service -> {
+            if (service.getServiceInterface().getPackage().getName().startsWith(servicesPackage)
+                    || !service.getMarkers().isEmpty() || service.getServiceInterface().getName().contains("tapestry5")) {
+                Object proxy = registry.getService(service.getServiceId(), (Class<?>) service.getServiceInterface());
+                beanFactory.registerResolvableDependency(service.getServiceInterface(), proxy);
+                logger.debug("TB: tapestry service {} exposed to spring", service.getServiceId());
+            }
+        });
+        beanFactory.registerResolvableDependency(Registry.class, registry);
+        logger.info("TB: tapestry Registry registered with spring (Still pending initialization)");
     }
 }
